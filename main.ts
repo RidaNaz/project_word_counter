1f55f3e8ab10df4dc1e70f9b9a4c49f77f1ad1fd
RidaNaz <rnaz3414@gmail.com>
2024-01-23 21:53:58 +0500

    initial commit

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 
 import inquirer from "inquirer";
 
-const counter = (text: string) => text.replace(/\s/g, "").length
+const counter = (text: string) => text.replace(/\s/g, "").length;
 
 async function start(counter: (text: string) => number) {
     do {
@@ -10,10 +10,9 @@ async function start(counter: (text: string) => number) {
             type: "input",
             name: "text",
             message: "Write your paragraph here :"
-        })
-        console.log(counter(userInput.text))
-    }
-    while (true)
-}
+        });
+        console.log(counter(userInput.text));
+    } while (true);
+};
 
-start(counter)
+start(counter);
